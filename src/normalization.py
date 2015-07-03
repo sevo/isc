368017a0e843d8fe9a4eb8ff9b8cbb5f2a4ea741
Jakub Sevcech <sevo_jakub@yahoo.fr>
2015-07-03 08:48:26 +0200

    normalizations updated to support initialization and coefficient reuse for multiple processed time series

--- src/normalization.py
+++ src/normalization.py
@@ -1,19 +1,88 @@
 import numpy as np
 
-class ZNormalization:
+class Normalization:
 
-    # returns [normalized value, scale shift, scale multiple ]
+    @classmethod
+    def base_normalization(cls, series, shift, scale):
+        """
+        generic time series normalization
+        :param series: time series
+        :param shift: shift to normalize by
+        :param scale: scale to normalize by
+        :return: [normalized series, shift, scale]
+        """
+        return [(series - shift)/scale, shift, scale]
+
+    def __init__(self):
+        """
+        the normalization object is used when we do not want to determine normalization coefficients for every
+        time series, but rather to have common coefficients for multiple time series
+        """
+        self.shift = 0
+        self.scale = 1
+
+    def set_coefficients(self, shift, scale):
+        """
+        set coefficients manually
+        :param shift: shift
+        :param scale: scale
+        """
+        self.shift = shift
+        self.scale = scale
+
+    def train_coefficients(self, series):
+        """
+        trains coefficients from time series
+        :param series: time series
+        """
+        self.shift, self.scale = self.__class__.coefficients(series)
+
+    def normalize(self, series):
+        """
+        normalization using stored coefficients
+        :param series: time series
+        :return: [normalized series, shift, scale]
+        """
+        return self.__class__.base_normalization(series, self.shift, self.scale)
+
+    @classmethod
+    def coefficients(cls, series):
+        """
+        :param series: time series
+        :return: [shift, scale]
+        """
+        return [0, 1]
+
     @classmethod
     def normalize(cls, series):
+        """
+        normalization with coefficient estimation
+        :param series: time series
+        :return: [normalized value, scale shift, scale multiple ]
+        """
+        shift, scale = cls.coefficients(series)
+        return cls.base_normalization(series, shift, scale)
+
+class ZNormalization(Normalization):
+
+    @classmethod
+    def coefficients(cls, series):
+        """
+        :param series: time series
+        :return: [shift, scale]
+        """
         mean_value = np.mean(series)
         std_value = np.std(series)
-        return [(series - mean_value) / std_value, mean_value, std_value]
+        return [mean_value, std_value]
 
-class MaxNormalization:
+class MaxNormalization(Normalization):
 
-    # returns [normalized value, scale shift, scale multiple ]
     @classmethod
-    def normalize(cls, series):
+    def coefficients(cls, series):
+        """
+        :param series: time series
+        :return: [shift, scale]
+        """
         min_value = np.min(series)
         max_value = np.max(series)
-        return [(series - min_value) / (max_value - min_value), min_value, max_value-min_value]
+        return [min_value, max_value - min_value]
--- test/test_normalization.py
+++ test/test_normalization.py
@@ -14,3 +14,21 @@ def test_max_normalization():
     assert normalized.equals(pd.Series([0, 0.5, 1]))
     assert scale_shift == 3
     assert scale_multiple == 2
+
+def test_instance_z_normalization():
+    series = pd.Series([3, 4, 5])
+    norm = normalization.ZNormalization()
+    norm.train_coefficients(series)
+    normalized, scale_shift, scale_multiple = norm.normalize(series)
+    assert normalized.equals(pd.Series([-1.2247448713915889, 0, 1.2247448713915889]))
+    assert scale_shift == 4
+    assert scale_multiple == 0.81649658092772603
+
+def test_instance_max_normalization():
+    series = pd.Series([3, 4, 5])
+    norm = normalization.MaxNormalization()
+    norm.train_coefficients(series)
+    normalized, scale_shift, scale_multiple = norm.normalize(series)
+    assert normalized.equals(pd.Series([0, 0.5, 1]))
+    assert scale_shift == 3
+    assert scale_multiple == 2
